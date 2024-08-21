2c6dc4f0b1d79e4941d9263c912627a229609823
Kevin Sidwar <sidwarkd@hardlysoftware.com>
2024-08-21 11:26:35 +0000

    chore: add dtf sdk as submodule

--- main/main.cpp
+++ main/main.cpp
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <thread>
 #include <chrono>
+#include "dtf_logging.h"
 
 extern "C" void app_main(void)
 {
+  dtf_set_log_level(INFO);
+
   while (true) {
-      printf("Welcome to the DTF SDK Playground\n");
+      DTF_LOGI("main", "Welcome to the DTF SDK Playground!");
       std::this_thread::sleep_for(std::chrono::seconds{5});
   }
 }
